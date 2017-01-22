de18f96d99ab631a5813bbdfcb42649fdf0cc0fc
Jack Stephens <jack@jrcs3.com>
2017-01-21 20:33:24 -0800

    Update package def to run in Heroku: Use enviromental varables to confit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use('/users', UserRouter);
 app.use('/courses', CourseRouter);  
 app.use('/enrollment', EnrollmentRouter);  
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log("Example app listening at http://%s;%s", host, port);
